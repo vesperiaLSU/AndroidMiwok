a651df3e6c40e973eac90f115ed7c81ed1d58d55
Chen Guo <guochen1223@gmail.com>
2016-09-14 10:56:05 -0500

    up button implemented

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         TextView numbers = (TextView) findViewById(R.id.numbers);
         numbers.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +55,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //Respond to the action bar's up/home button
+            case android.R.id.home:
+                Intent intent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, intent)) {
+                    //This activity is not part of this app's task, so create a new task
+                    //when navigating up, with a synthesized back stack
+                    //Add all of this activity's parents to the back stack
+                    //Navigate up to the closest parent
+                    TaskStackBuilder.create(this).addNextIntentWithParentStack(intent).startActivities();
+                } else {
+                    NavUtils.navigateUpTo(this, intent);
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
