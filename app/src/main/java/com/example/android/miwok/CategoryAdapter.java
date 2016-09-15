5e808341c17b182c94289eacb8ea70c5998a81ab
Chen Guo <guochen1223@gmail.com>
2016-09-15 16:44:40 -0500

    view pager with adapter and tabs layout

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,12 +1,18 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
-    public CategoryAdapter(FragmentManager fm) {
+
+    // Context of the app
+    private Context context;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        this.context = context;
     }
 
     @Override
@@ -28,5 +34,21 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return context.getString(R.string.category_numbers);
+            case 1:
+                return context.getString(R.string.category_family);
+            case 2:
+                return context.getString(R.string.category_colors);
+            case 3:
+                return context.getString(R.string.category_phrases);
+            default:
+                return context.getString(R.string.category_numbers);
+        }
     }
 }
